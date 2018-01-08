413195efedc48d70c22b7345f29384880294096f
Alvin <romascudeto@yahoo.com>
2018-01-08 23:24:15 +0700

    Change #1

--- server/boot/bot.js
+++ server/boot/bot.js
@@ -1,14 +1,11 @@
 var SlackBot = require('slackbots');
 
-// create a bot
 var bot = new SlackBot({
-   token: 'xoxb-294856326721-h7xEyZ978KSsUWpyBiCCeAUq', // Add a bot https://my.slack.com/services/new/bot and put the token 
+   token: 'xoxb-294856326721-h7xEyZ978KSsUWpyBiCCeAUq', 
    name: 'Karmabot'
 });
 
 bot.on('message', function(data) {
-    console.log(data.channel);
-    // all ingoing events https://api.slack.com/rtm
     if(data.type == "message"){
         if (data.text == "test"){
             bot.postMessage(data.channel, 'meow!');                    
