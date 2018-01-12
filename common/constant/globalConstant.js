48ef3ce9f18580322b600a19de3e67e6e6eab3b1
Alvin <romascudeto@yahoo.com>
2018-01-12 17:13:23 +0700

    Change Port

--- common/constant/globalConstant.js
+++ common/constant/globalConstant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BOT_TOKEN: 'xoxb-294856326721-uQ6PcNsRoD01HbAUrAdVjoH4',
+    BOT_TOKEN: 'xoxb-294856326721-BZlxRZXzPxyka8gJwMje5t20',
     BOT_NAME: 'karmabot',
     DATA_TYPE : 'message',
     DATA_KEYWORD : 'thanks',
